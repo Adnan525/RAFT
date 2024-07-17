7d8339a35d44c7c6aa108b26bd3592a8b8528c3b
Muntasir Adnan <48549305+Adnan525@users.noreply.github.com>
2024-07-17 13:19:41 +0000

    RAFT dataset created

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from tqdm.auto import tqdm
 
 from llama_index.llms.openai import OpenAI
 from llama_index.embeddings.openai import OpenAIEmbedding
@@ -18,7 +19,7 @@ docs = get_doc_chunk("./text/")
 
 # generate questions for each chunk and save the questions in questions.txt
 with open("questions.txt", "a") as f:
-    for i, chunk in enumerate(docs):
+    for i, chunk in tqdm(enumerate(docs)):
         openai_response = generate_qa_pair(llm, chunk)
         # each chunk will generate 2 questions
         question_list = process_qa_pair(openai_response)
@@ -26,14 +27,23 @@ with open("questions.txt", "a") as f:
             f.write(text + "\n")
             if j%2 == 1:
                 f.write(str(i) + "\n")
-        break
+
+print(f"[INFO] All Questions Generated")
 
 # generate CoT answer for each question
 question_file = open("questions.txt", "r")
 q_a_i = question_file.readlines()
+
+q_count = 0
 with open("raft.txt", "a") as raft_file:
     for i, line in enumerate(q_a_i):
         raft_file.write(line)
+        raft_file.write("-"*5 + "\n")
         # if a question generate CoT
         if i%3 == 0:
-            generate_cot_answer(llm, line, int(q_a_i[i+2]))
+            print(f"[INFO] CoT explanantion generated for Question {q_count}")
+            chunk_index = int(q_a_i[i+2])
+            cot = generate_cot_answer(llm, line, docs[chunk_index])
+            raft_file.write(cot + "\n")
+            raft_file.write("-"*5 + "\n")
+            q_count+=1
